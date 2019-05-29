bcaef84f00107cc0f5796a760577fa401cb4fa67
kulro03 <kulro03@ca.com>
2019-05-29 15:35:18 +0200

    Intermediate changes to synchronize work

--- src/main/java/com/rok/solver/Main.java
+++ src/main/java/com/rok/solver/Main.java
@@ -1,6 +1,8 @@
 package com.rok.solver;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
@@ -8,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] field = {
-                {-1,1,3, 8, -1, -1, 4, -1, 5},
+                {-1, 1, 3, 8, -1, -1, 4, -1, 5},
                 {-1, 2, 4, 6, -1, 5, -1, -1, -1},
                 {-1, 8, 7, -1, -1, -1, 9, 3, -1},
                 {4, 9, -1, 3, -1, 6, -1, -1, -1},
@@ -25,26 +27,42 @@ public class Main {
     private static void solve(int[][] field) {
         Field field1 = new Field(field);
         field1.calculateRatios();
+        List<AreaRatio> rowRatios = field1.getRowRatios();
+
+        for (AreaRatio ratio : rowRatios ) {
+            tryFillArea(ratio.areaIndex, ratio.areaType);
+        }
+
         field1.printRatios();
+    }
+
+    private static void tryFillArea(int areaIndex, AreaRatio.AreaType areaType) {
+
     }
 
 
     static class Field {
         final int[][] field;
-        private double[] rowRatios = new double[MAGIC_SUDOKU_NUMBER];
-        private double[] columnRatios  = new double[MAGIC_SUDOKU_NUMBER];
-        private double[] blockRatios  = new double[MAGIC_SUDOKU_NUMBER];
+
+        private List<AreaRatio> rowRatios = new ArrayList<>(MAGIC_SUDOKU_NUMBER);
+//        private double[] columnRatios  = new double[MAGIC_SUDOKU_NUMBER];
+//        private double[] blockRatios  = new double[MAGIC_SUDOKU_NUMBER];
 
         Field(int[][] field) {
             this.field = field;
         }
 
 
+        public List<AreaRatio> getRowRatios() {
+            return rowRatios;
+        }
+
+
         public void calculateRatios() {
-            for (int i = 0; i< MAGIC_SUDOKU_NUMBER; i++) {
+            for (int i = 0; i < MAGIC_SUDOKU_NUMBER; i++) {
                 int notEmptyInRow = 0;
                 int notEmptyInColumn = 0;
-                for (int j = 0; j< MAGIC_SUDOKU_NUMBER; j++) {
+                for (int j = 0; j < MAGIC_SUDOKU_NUMBER; j++) {
                     if (field[i][j] != -1) {
                         notEmptyInRow++;
                     }
@@ -52,15 +70,47 @@ public class Main {
                         notEmptyInColumn++;
                     }
                 }
-                rowRatios[i] = 1. * notEmptyInRow/MAGIC_SUDOKU_NUMBER;
-                columnRatios[i] = 1. * notEmptyInColumn/MAGIC_SUDOKU_NUMBER;
+                rowRatios.add(new AreaRatio(i, 1. * notEmptyInRow / MAGIC_SUDOKU_NUMBER, AreaRatio.AreaType.ROW));
+                //columnRatios[i] = 1. * notEmptyInColumn/MAGIC_SUDOKU_NUMBER;
             }
+
+            rowRatios.sort(Collections.reverseOrder());
         }
 
         public void printRatios() {
-            Arrays.stream(rowRatios).forEach(i -> System.out.print(i+ " "));
+            rowRatios.forEach(System.out::println);
             System.out.println();
-            Arrays.stream(columnRatios).forEach(i -> System.out.print(i+ " "));
+//            Arrays.stream(columnRatios).forEach(i -> System.out.print(i+ " "));
+        }
+    }
+
+
+    public static class AreaRatio implements Comparable<AreaRatio> {
+
+        enum AreaType {
+            ROW, COLUMN, BLOCK
+        }
+
+        private int areaIndex;
+        private double ratio;
+        private AreaType areaType;
+
+        public AreaRatio(int areaIndex, double ratio, AreaType areaType) {
+
+            this.areaIndex = areaIndex;
+            this.ratio = ratio;
+            this.areaType = areaType;
+        }
+
+        @Override
+        public int compareTo(AreaRatio o) {
+            return Double.compare(this.ratio, o.ratio);
+        }
+
+        @Override
+        public String toString() {
+            return areaType + ": index=" + areaIndex +
+                    ", ratio=" + ratio + '}';
         }
     }
 }
